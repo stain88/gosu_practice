dcd72f428c41d4930abb501f853a5fd51445f508
stain88 <marcbaghdadi@gmail.com>
2016-10-16 20:51:37 +0100

    Game window

---
+++ pong/Main.rb
@@ -0,0 +1,25 @@
+require 'gosu'
+
+class GameWindow < Gosu::Window
+	def initialize
+		super 860,640
+		self.caption = "Pong"
+	end
+
+	def update
+
+	end
+
+	def draw
+
+	end
+
+	def button_down id
+		if id == Gosu::KbEscape
+			close
+		end
+	end
+end
+
+window = GameWindow.new
+window.show
