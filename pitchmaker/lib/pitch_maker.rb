0ee3284f2eaff8bc761db61687d3a510203d69ee
stain88 <marcbaghdadi@gmail.com>
2017-01-25 15:07:11 +0000

    define initial drawing functions

--- pitchmaker/lib/pitch_maker.rb
+++ pitchmaker/lib/pitch_maker.rb
@@ -15,4 +15,54 @@ class PitchMaker < Gosu::Window
 
 		@font = Gosu::Font.new(self, Gosu::default_font_name, 20)
 	end
+
+	def update
+
+	end
+
+	def draw
+		draw_background
+		display_instructions
+	end
+
+	def button_down id
+		case id
+		when Gosu::KbEscape, Gosu::KbQ
+			puts "Exiting #{TITLE}"
+			close
+		when Gosu::MsLeft, Gosu::MsRight
+			puts "Mouse button down"
+		end
+	end
+
+	def button_up id
+		case id
+		when Gosu::MsLeft, Gosu::MsRight
+			puts "Mouse button up"
+		end
+	end
+
+	private
+
+		def draw_background
+			c = Gosu::Color::BLACK
+			z_index = -100
+			draw_quad(0, 0, c, width, 0, c, 0, height, c, width, height, c, z_index)
+		end
+
+		def display_instructions
+			c = Gosu::Color::CYAN
+			top_margin = 10
+			left_margin = 10
+			line_height = 20
+			z_index = 200
+			[
+				"Click mouse to play tone",
+				"Move mouse to change pitch/volume",
+				"Press 'q' to exit"
+			].each_with_index do |text, i|
+				text_y = top_margin + (i*line_height)
+				@font.draw(text, left_margin, text_y, z_index+i, 1.0, 1.0, c)
+			end
+		end
 end
